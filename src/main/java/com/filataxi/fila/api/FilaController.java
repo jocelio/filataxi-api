6331bf8980c4d87d42d78075494ec3361bd535d4
jocelio <jocelio27@gmail.com>
2018-03-26 17:30:52 -0300

    creating queue for the a certain next day that is passed.

--- src/main/java/com/filataxi/fila/api/FilaController.java
+++ src/main/java/com/filataxi/fila/api/FilaController.java
@@ -12,11 +12,14 @@ import org.springframework.web.bind.annotation.*;
 import reactor.bus.Event;
 import reactor.bus.EventBus;
 
+import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.IntStream;
 
 import static com.filataxi.fila.model.Status.AGUARDANDO;
 import static com.filataxi.fila.model.Status.RODANDO;
+import static java.time.LocalDate.now;
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.toList;
 
@@ -35,13 +38,13 @@ public class FilaController {
 
 	@GetMapping
 	public List<Position> get() {
-		return positionRepository.findAllByOrderByIndexAsc();
+		return positionRepository.findAllByDateOrderByIndexAsc(now());
 	}
 
 	@PostMapping("head")
 	public List<Position> head() {
 
-		List<Position> all = positionRepository.findAllByOrderByIndexAsc();
+		List<Position> all = positionRepository.findAllByDateOrderByIndexAsc(now());
 
 		Position position = all.stream().sorted(comparing(Position::getIndex)).findFirst().get();
 		Position lastIndex = all.stream().sorted(comparing(Position::getIndex).reversed()).findFirst().get();
@@ -59,7 +62,7 @@ public class FilaController {
 
 		positionRepository.save(updates);
 
-		return positionRepository.findAllByOrderByIndexAsc();
+		return positionRepository.findAllByDateOrderByIndexAsc(now());
 	}
 
 
@@ -72,7 +75,7 @@ public class FilaController {
 		List<Driver> all = driverRepository.findByEnabledTrue();
 
 		List<Position> positions = IntStream.range(0, all.size())
-				.mapToObj(o -> Position.builder().driver(all.get(o)).index(++o).status(AGUARDANDO).build())
+				.mapToObj(o -> Position.builder().date(now()).driver(all.get(o)).index(++o).status(AGUARDANDO).build())
 				.collect(toList());
 
 		return positionRepository.save(positions);
@@ -82,7 +85,7 @@ public class FilaController {
 	@PostMapping("move-up/{id}/{qtyPositions}")
 	public List<Position> moveUp(@PathVariable Integer id, @PathVariable Integer qtyPositions) {
 
-		List<Position> all = positionRepository.findAllByOrderByIndexAsc();
+		List<Position> all = positionRepository.findAllByDateOrderByIndexAsc(now());
 
 		Position position = all.stream().filter(p -> p.getId().equals(id)).findAny().get();
 
@@ -104,7 +107,7 @@ public class FilaController {
 
 		positionRepository.save(updates);
 
-		return positionRepository.findAllByOrderByIndexAsc();
+		return positionRepository.findAllByDateOrderByIndexAsc(now());
 
 	}
 
@@ -117,6 +120,32 @@ public class FilaController {
 		eventBus.notify("historyConsumer", Event.wrap(HistoryData.changeStatus(position)));
 
 		return positionRepository.save(position);
+	}
+
+	@PostMapping("next-queue/{days}")
+	public List<Position> nextQueue(@PathVariable Integer days) {
+
+		List<Position> today = positionRepository.findAllByDateOrderByIndexAsc(now());
+
+		List<Position> newQueue = today.stream()
+				.filter(f -> f.getStatus().equals(AGUARDANDO))
+				.map(p -> Position.builder()
+						.date(LocalDate.now().plusDays(days))
+						.index(p.getIndex())
+						.driver(p.getDriver())
+						.status(AGUARDANDO)
+						.build())
+				.sorted(Comparator.comparing(Position::getIndex))
+				.collect(toList());
+
+		List<Position> positions = IntStream.range(0, newQueue.size()).mapToObj(i -> newQueue.get(i).withIndex(i + 1)).collect(toList());
+
+		positions.forEach(p -> {
+				eventBus.notify("historyConsumer", Event.wrap(HistoryData.nextQueue(p)));
+		});
+
+		return positionRepository.save(positions);
+
 	}
 
 
