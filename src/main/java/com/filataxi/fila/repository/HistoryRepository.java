2a14bb52b4c6fbb09b90ddfc2e62caccda9c3065
jocelio <jocelio27@gmail.com>
2018-03-26 17:29:54 -0300

    getting driver from email.

--- src/main/java/com/filataxi/fila/repository/HistoryRepository.java
+++ src/main/java/com/filataxi/fila/repository/HistoryRepository.java
@@ -4,7 +4,10 @@ import com.filataxi.fila.model.HistoryData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface HistoryRepository extends JpaRepository<HistoryData, Integer> {
 
+		List<HistoryData> findAllByIdDriver(Integer idDriver);
 }
