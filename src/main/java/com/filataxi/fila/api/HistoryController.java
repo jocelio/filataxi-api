142e5d92fc771e285d8f001d98bc7fcffd5bcb63
jocelio <jocelio27@gmail.com>
2018-03-26 17:29:00 -0300

    returning history from a certain driver.

--- src/main/java/com/filataxi/fila/api/HistoryController.java
+++ src/main/java/com/filataxi/fila/api/HistoryController.java
@@ -4,6 +4,7 @@ import com.filataxi.fila.model.HistoryData;
 import com.filataxi.fila.repository.HistoryRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class HistoryController {
 	@GetMapping
 	public List<HistoryData> get() {
 		return historyRepository.findAll();
+	}
+
+	@GetMapping("/{idDriver}")
+	public List<HistoryData> byIdDriver(@PathVariable Integer idDriver) {
+		return historyRepository.findAllByIdDriver(idDriver);
 	}
 }
